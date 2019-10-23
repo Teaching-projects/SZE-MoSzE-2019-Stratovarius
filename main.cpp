39e6b74ece4afde086f365ec126f9afd6a3dec05
szilidaniel <34722259+szilidaniel@users.noreply.github.com>
2019-10-23 17:53:58 +0200

    Feature 1 complete
    A feladatot teljesíti bár van még mit javítani rajta

--- main.cpp
+++ main.cpp
@@ -13,12 +13,12 @@ bool validcommand(string command) {
 
 vector<string> split(string path) {
 	vector<string> directories;
-	int cut=0;
+	int cut = 0;
 	while (cut < 4294967290) {
 		cut = path.find_first_of("/");
-		string element = path.substr(0,cut);
+		string element = path.substr(0, cut);
 		directories.push_back(element);
-		path= path.substr(cut+1,path.size());
+		path = path.substr(cut + 1, path.size());
 	}
 
 	return directories;
@@ -50,54 +50,62 @@ int main() {
 		if (!validcommand(parancs)) {
 			cout << "'" << parancs << "' is not recognized as an internal or external command, operable program or batch file." << endl;
 		}
-		if (parancs == "mkdir") {
-			string dirname;
-			cin >> dirname;
-			d.mkdir(dirname, CurrentFolder);
-		}
-		if (parancs == "cd") {
-			string dirname;
-			cin >> dirname;
-			vector<string> path;
-			if (dirname.find_first_of("/") < 4294967290) {
-				path = split(dirname);
+		else {
+			if (parancs == "ls") {
+				d.ls(CurrentFolder);
 			}
-			else {				
-				path.push_back(dirname);
-			}
-			for (unsigned int i = 0; i < path.size(); i++) {
-				if (path[i] == "..") {
-					if (CurrentFolder != "root") {
-						int cut = CurrentFolder.find_last_of("/");
-						CurrentFolder = CurrentFolder.substr(0, cut);
+			else {
+				string dirname;
+				cin >> dirname;
+				vector<string> path;
+				if (dirname.find_first_of("/") < 4294967290) {
+					path = split(dirname);
+					path.pop_back();
+					dirname = dirname.substr(dirname.find_last_of("/") + 1, dirname.size());
+					for (unsigned int i = 0; i < path.size(); i++) {
+						if (path[i] == "..") {
+							if (CurrentFolder != "root") {
+								int cut = CurrentFolder.find_last_of("/");
+								CurrentFolder = CurrentFolder.substr(0, cut);
+							}
+							else {
+								cout << "You're already in the root directory." << endl;
+							}
+						}
+						else {
+							CurrentFolder = d.cd(path[i], CurrentFolder);
+						}
+					}
+				}
+				if (parancs == "mkdir") {
+					d.mkdir(dirname, CurrentFolder);
+				}
+				if (parancs == "cd") {
+					if (dirname == "..") {
+						if (CurrentFolder != "root") {
+							int cut = CurrentFolder.find_last_of("/");
+							CurrentFolder = CurrentFolder.substr(0, cut);
+						}
+						else {
+							cout << "You're already in the root directory." << endl;
+						}
 					}
 					else {
-						cout << "You're already in the root directory." << endl;
+						CurrentFolder = d.cd(dirname, CurrentFolder);
 					}
-				}
-				else {
-					CurrentFolder = d.cd(path[i], CurrentFolder);
-				}
-			}
-			
-		}
 
-		if (parancs == "ls") {
-			d.ls(CurrentFolder);
-		}
-		if (parancs == "rm") {
-			string dirname;
-			cin >> dirname;
-			if (dirname == "-rf") {
-				cin >> dirname;
-				d.rmForce(dirname, CurrentFolder);
+				}
+				if (parancs == "rm") {
+					if (dirname == "-rf") {
+						cin >> dirname;
+						d.rmForce(dirname, CurrentFolder);
+					}
+					else d.rm(dirname, CurrentFolder);
+				}
+				if (parancs == "touch") {
+					d.touch(dirname, CurrentFolder);
+				}
 			}
-			else d.rm(dirname, CurrentFolder);
-		}
-		if (parancs == "touch") {
-			string fileName;
-			cin >> fileName;
-			d.touch(fileName, CurrentFolder);
 		}
 	}
 	return 0;
